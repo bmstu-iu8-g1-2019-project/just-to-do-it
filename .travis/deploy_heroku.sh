92c59591510b160432d39b1b769226858031894b
dankokin <d_kokin@inbox.ru>
2019-10-03 21:39:00 +0200

    Fixed mistake

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -6,4 +6,4 @@ echo "$HEROKU_API_KEY" | docker login -u _  --password-stdin registry.heroku.com
 
 docker build --tag=registry.heroku.com/${HEROKU_APP_NAME}/web .
 docker push registry.heroku.com/${HEROKU_APP_NAME}/web
-heroku container:release web --app ${HEROKU_APP_NAME}e web --app ${HEROKU_APP_NAME}
+heroku container:release web --app ${HEROKU_APP_NAME}
