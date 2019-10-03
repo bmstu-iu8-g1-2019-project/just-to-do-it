08a501b71a9be2f9a32372ee22653abacffc72b5
dankokin <d_kokin@inbox.ru>
2019-10-03 21:26:39 +0200

    Fix: script and travis command

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-heroku plugins:install heroku-container-registry
+heroku plugins:install @heroku-cli/plugin-container-registry
 
-docker login -e _ -u _ --password=$HEROKU_API_KEY registry.heroku.com
+echo "$HEROKU_API_KEY" | docker login -u _  --password-stdin registry.heroku.com
 
-heroku container:push web --app $HEROKU_APP_NAME
-heroku container:release web --app ${HEROKU_APP_NAME}
+docker build --tag=registry.heroku.com/${HEROKU_APP_NAME}/web .
+docker push registry.heroku.com/${HEROKU_APP_NAME}/web
+heroku container:release web --app ${HEROKU_APP_NAME}e web --app ${HEROKU_APP_NAME}
