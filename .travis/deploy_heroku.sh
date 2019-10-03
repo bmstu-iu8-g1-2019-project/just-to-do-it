e49971ff2851ca7238018a3082bf8765a1a0d1ed
dankokin <d_kokin@inbox.ru>
2019-10-03 20:57:28 +0200

    2nd try

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -4,4 +4,7 @@ wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku plugins:install heroku-container-registry
 
 docker login -e _ -u _ --password=$HEROKU_API_KEY registry.heroku.com
-heroku container:push web --app $HEROKU_APP_NAME
+
+docker build --tag=registry.heroku.com/${HEROKU_APP_NAME}/web .
+docker push registry.heroku.com/${HEROKU_APP_NAME}/web
+heroku container:release web --app ${HEROKU_APP_NAME}
