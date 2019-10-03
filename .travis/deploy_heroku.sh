c4a5ead621c0febe8882883a0a5c7cf3f57b2424
dankokin <d_kokin@inbox.ru>
2019-10-03 21:18:44 +0200

    Fix: deploy to heroku

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -5,6 +5,5 @@ heroku plugins:install heroku-container-registry
 
 docker login -e _ -u _ --password=$HEROKU_API_KEY registry.heroku.com
 
-docker build --tag=registry.heroku.com/${HEROKU_APP_NAME}/web .
-docker push registry.heroku.com/${HEROKU_APP_NAME}/web
+heroku container:push web --app $HEROKU_APP_NAME
 heroku container:release web --app ${HEROKU_APP_NAME}
