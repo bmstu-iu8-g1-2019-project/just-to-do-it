65355bd2a470ecc62bef236ca688dc365a918775
sergejkoll <kolesnikov.school4@gmail.com>
2019-10-21 16:29:06 +0300

    fixed tab))

--- src/main/main.go
+++ src/main/main.go
@@ -11,34 +11,34 @@ import (
 )
 
 func main() {
-	db, err := services.NewDB(services.OpenConfigFile("config.txt"))
-	if err != nil {
-		log.Panic(err)
-	}
+    db, err := services.NewDB(services.OpenConfigFile("config.txt"))
+    if err != nil {
+        log.Panic(err)
+    }
 
-        envUser := &controllers.EnvironmentUser{ db}
-	envTask := &controllers.EnvironmentTask{ db}
+    envUser := &controllers.EnvironmentUser{ db}
+    envTask := &controllers.EnvironmentTask{ db}
 
-	r := mux.NewRouter()
-	r.Use(SetJSONHeader)
+    r := mux.NewRouter()
+    r.Use(SetJSONHeader)
 
-	r.HandleFunc("/user/task/{id}", envTask.GetTaskTIdHandler).Methods("GET")
-	r.HandleFunc("/user/task/{assignee_id}", envTask.GetTasksAIdHandler).Methods("GET")
-	r.HandleFunc("/user/task/{group_id}", envTask.GetTasksGIdHandler).Methods("GET")
-	r.HandleFunc("/user/task", envTask.CreateTask).Methods("POST")
-	r.HandleFunc("/user/task/{id}", envTask.UpdateTask).Methods("PUT")
-        r.HandleFunc("/login", envUser.ResponseLoginHandler).Methods("GET")
-	r.HandleFunc("/register", envUser.ResponseRegisterHandler).Methods("POST")
-	r.HandleFunc("/confirm", envUser.ConfirmEmailHandler).Methods("GET")
-	r.HandleFunc("/user/{id}", envUser.UpdateUserHandler).Methods("PUT")
-	r.HandleFunc("/user/{id}", envUser.GetUserHandler).Methods("GET")
-	r.HandleFunc("/user/{id}", envUser.DeleteUserHandler).Methods("DELETE")
-        http.ListenAndServe(":3000", r)
+    r.HandleFunc("/user/task/{id}", envTask.GetTaskTIdHandler).Methods("GET")
+    r.HandleFunc("/user/task/{assignee_id}", envTask.GetTasksAIdHandler).Methods("GET")
+    r.HandleFunc("/user/task/{group_id}", envTask.GetTasksGIdHandler).Methods("GET")
+    r.HandleFunc("/user/task", envTask.CreateTask).Methods("POST")
+    r.HandleFunc("/user/task/{id}", envTask.UpdateTask).Methods("PUT")
+    r.HandleFunc("/login", envUser.ResponseLoginHandler).Methods("GET")
+    r.HandleFunc("/register", envUser.ResponseRegisterHandler).Methods("POST")
+    r.HandleFunc("/confirm", envUser.ConfirmEmailHandler).Methods("GET")
+    r.HandleFunc("/user/{id}", envUser.UpdateUserHandler).Methods("PUT")
+    r.HandleFunc("/user/{id}", envUser.GetUserHandler).Methods("GET")
+    r.HandleFunc("/user/{id}", envUser.DeleteUserHandler).Methods("DELETE")
+    http.ListenAndServe(":3000", r)
 }
 
 func SetJSONHeader(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		h.ServeHTTP(w, r)
-	})
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Content-Type", "application/json; charset=utf-8")
+        h.ServeHTTP(w, r)
+    })
 }
