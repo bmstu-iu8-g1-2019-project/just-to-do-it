928b7584c88118eefd8b607240ba5b6d63e6effe
dankokin <d_kokin@inbox.ru>
2019-10-25 01:41:35 +0200

    added services-tests

--- src/services/db_service.go
+++ src/services/db_service.go
@@ -3,6 +3,12 @@ package services
 import (
 	"database/sql"
 	"fmt"
+	"io"
+	"os"
+	"regexp"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/bmstu-iu8-g1-2019-project/just-to-do-it/src/models"
 )
 
 // structure for functions that access the database
@@ -10,20 +16,8 @@ type DB struct {
 	*sql.DB
 }
 
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "pass"
-	dbname   = "postgres"
-)
-
-func NewDB() (*DB, error) {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
-	// open connection
-	db, err := sql.Open("postgres", psqlInfo)
+func NewDB(dbSourceName string) (*DB, error) {
+	db, err := sql.Open("postgres", dbSourceName)
 	if err != nil {
 		return nil, err
 	}
@@ -32,5 +26,99 @@ func NewDB() (*DB, error) {
 		return nil, err
 	}
 	fmt.Println("Successfully connected!")
+	return &DB{db}, nil
+}
+
+func OpenConfigFile(filename string) (config string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	data := make([]byte, 64)
+	for {
+		n, err := file.Read(data)
+		if err == io.EOF {
+			break
+		}
+		config += string(data[:n])
+	}
+	return config
+}
+
+func NewMockGetDB(users []models.User) (*DB, error) {
+	db, mock, err := sqlmock.New()
+
+	if err != nil {
+		return &DB{}, err
+	}
+	rows := sqlmock.NewRows([]string{"id", "email", "login", "fullname",
+		"password", "acc_verified"})
+	for i := range users {
+		expectedUser := users[i]
+		rows.AddRow(
+			expectedUser.Id,
+			expectedUser.Email,
+			expectedUser.Login,
+			expectedUser.Fullname,
+			expectedUser.Password,
+			expectedUser.AccVerified)
+
+		mock.ExpectQuery(regexp.QuoteMeta("SELECT * FROM user_table WHERE id = $1")).
+			WithArgs(expectedUser.Id).WillReturnRows(rows)
+	}
+	return &DB{db}, nil
+}
+
+func NewMockLoginDB(users []models.User, hash []string) (*DB, error) {
+	db, mock, err := sqlmock.New()
+
+	if err != nil {
+		return &DB{}, err
+	}
+	rows := sqlmock.NewRows([]string{"id", "email", "login", "fullname",
+		"password", "acc_verified"})
+	for i := range users {
+		expectedUser := users[i]
+		rows.AddRow(
+			expectedUser.Id,
+			expectedUser.Email,
+			expectedUser.Login,
+			expectedUser.Fullname,
+			hash[i],
+			expectedUser.AccVerified)
+
+		mock.ExpectQuery(regexp.QuoteMeta("SELECT * FROM user_table WHERE login = $1")).
+			WithArgs(expectedUser.Login).WillReturnRows(rows)
+	}
+	return &DB{db}, nil
+}
+
+func NewMockDeleteDB(users []models.User, ids []int) (*DB, error) {
+	db, mock, err := sqlmock.New()
+
+	if err != nil {
+		return &DB{}, err
+	}
+	rows := sqlmock.NewRows([]string{"id", "email", "login", "fullname",
+		"password", "acc_verified"})
+	for i := range users {
+		expectedUser := users[i]
+		rows.AddRow(
+			expectedUser.Id,
+			expectedUser.Email,
+			expectedUser.Login,
+			expectedUser.Fullname,
+			expectedUser.Password,
+			expectedUser.AccVerified)
+
+		mock.ExpectQuery(regexp.QuoteMeta("SELECT * FROM user_table WHERE id = $1")).
+			WithArgs(expectedUser.Id).WillReturnRows(rows)
+		mock.ExpectExec(regexp.QuoteMeta("DELETE FROM user_table WHERE id = $1")).
+			WithArgs(ids[i]).
+			WillReturnResult(sqlmock.NewResult(1, 1))
+	}
 	return &DB{db}, nil
 }
--- src/services/user_service.go
+++ src/services/user_service.go
@@ -136,3 +136,38 @@ func (db *DB) Confirm(hash string) (err error) {
 		return nil
 	}
 }
+
+func (db *DB) UpdateUser(id int, updateUser models.User) (err error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(updateUser.Password), 8)
+	_, err = db.Exec("UPDATE user_table SET email = $1, login = $2,"+
+		"fullname = $3, password = $4, acc_verified = $5"+
+		"where id = $6", updateUser.Email,
+		updateUser.Login, updateUser.Fullname, hashedPassword,
+		updateUser.AccVerified, id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (db *DB) GetUser(id int) (user models.User, err error) {
+	row := db.QueryRow("SELECT * FROM user_table WHERE id = $1", id)
+	err = row.Scan(&user.Id, &user.Email, &user.Login, &user.Fullname,
+		&user.Password, &user.AccVerified)
+	if err != nil {
+		return models.User{}, err
+	}
+	return user, nil
+}
+
+func (db *DB) DeleteUser(id int) (err error) {
+	user, err := db.GetUser(id)
+	if err != nil {
+		return err
+	}
+	_, err = db.Exec("DELETE  FROM user_table WHERE id = $1", user.Id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
